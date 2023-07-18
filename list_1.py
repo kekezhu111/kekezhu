3622042985320ab0afdab44c135c0001a3b0781c
kekezhu111 <16121532@qq.com>
2023-07-18 22:34:52 +0800

    判断子列表修改

--- list_1.py
+++ list_1.py
@@ -3,9 +3,9 @@ from typing import List
 
 class Solution:
     def pivotIndex(self, nums1: List[int], nums2: List[int]) -> bool:
-        a = b = 0
+        a = b = d = 0
         nums3 = []
-        for i in range(len(nums2)):
+        while b < (len(nums2)):
             if nums1[a] == nums2[b]:
                 nums3.append(nums2[b])
                 if nums3 == nums1:
@@ -14,17 +14,15 @@ class Solution:
                 b += 1
             else:
                 a = 0
-                i += 1
-                b = i
+                d += 1
+                b = d
                 nums3 = []
         else:
             return False
-        # print(nums3)
 
 
 c = Solution()
 print(c.pivotIndex([4, 1, 3, 5, 6], [1, 3, 2, 4, 1, 3, 5, 6]))
 print(c.pivotIndex([2, 4], [1, 3, 2, 4, 1, 3, 5]))
 print(c.pivotIndex([1, 3, 3], [1, 3, 2, 4, 1, 3, 5]))
-print(c.pivotIndex([5, 6, 7], [1, 2, 3, 4, 5, 6, 7, 8, 9]))
-
+print(c.pivotIndex([1, 1, 1, 2], [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 6, 7, 8, 9]))
